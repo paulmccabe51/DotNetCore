27400156ce49dd1873cb1e5a7db9475379e5e16e
Paul <paulmccabe51@hotmail.com>
2018-09-23 21:49:12 +0100

    Added readme, deploy and playing with exceptions

---
+++ DotNetCore/InvokeWebRequests.ps1
@@ -0,0 +1,11 @@
+﻿<#
+for ($i = 0 ; $i -lt 20; $i++)
+{
+ Invoke-WebRequest -uri http://localhost:44606/
+}
+#>
+
+for ($i = 0 ; $i -lt 20; $i++)
+{
+ Invoke-WebRequest -uri http://localhost:44606/Contact
+}
